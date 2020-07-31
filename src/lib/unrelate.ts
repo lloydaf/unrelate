9ef27cd2fa500dbe3bf699d3929f3b623a12ff2c
Lloyd Francis <lloydaf@gmail.com>
2020-07-31 13:45:38 +0530

    0.0.3

--- src/lib/unrelate.ts
+++ src/lib/unrelate.ts
@@ -1,15 +1,16 @@
 #!/usr/bin/env node
 import { configure } from './configure';
 import { cleanup } from './cleanup';
+import { Commands } from '../model/enums';
 
 const args = process.argv.splice(process.execArgv.length + 2);
 
 switch (args[0]) {
-  case 'configure': {
+  case Commands.CONFIGURE: {
     configure(args[1], args[2]);
     break;
   }
-  case 'cleanup': {
+  case Commands.CLEANUP: {
     cleanup(args[1]);
   }
 }
