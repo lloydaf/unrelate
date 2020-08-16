29d4d27123bb639fa6267b6d748354f16e222082
Lloyd Francis <lloydaf@gmail.com>
2020-08-16 12:00:01 +0530

    fixing failing test

--- src/util/__tests__/file.util.test.ts
+++ src/util/__tests__/file.util.test.ts
@@ -1,12 +1,12 @@
 import * as exported from '../file.util';
 
 describe('tests for config-file.util', () => {
-  it('should export configFileDataManager, fileDataManager, getFilePath, doesItExist, fileOrFolder, getFilesInDirectory', () => {
+  it('should export configFileDataManager, fileDataManager, getFilePath, doesItExist, fileOrFolder, getDirectoryItems', () => {
     expect(exported['configFileDataManager']).toBeTruthy();
     expect(exported['fileDataManager']).toBeTruthy();
     expect(exported['getFilePath']).toBeTruthy();
     expect(exported['doesItExist']).toBeTruthy();
     expect(exported['fileOrFolder']).toBeTruthy();
-    expect(exported['getFilesInDirectory']).toBeTruthy();
+    expect(exported['getDirectoryItems']).toBeTruthy();
   });
 });
