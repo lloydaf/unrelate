9a8301aa54faf27c2e9a320b20ddeaef8ddcacfd
Lloyd Francis <lloydaf@gmail.com>
2020-08-22 19:11:42 +0530

    Exporting addPath and configureBaseUrl methods, updating deps, fixing… (#15)
    * Exporting addPath and configureBaseUrl methods, updating deps, fixing formatting issues on README
    
    * Updating tests

--- src/lib/__tests__/configure.test.ts
+++ src/lib/__tests__/configure.test.ts
@@ -1,7 +1,9 @@
 import * as exported from '../configure';
 
 describe('tests for configure.ts', () => {
-  it('should export a function called configure', () => {
+  it('should export a function called configure, configureBaseUrl and addPath', () => {
     expect(exported['configure']).toBeTruthy();
+    expect(exported['addPath']).toBeTruthy();
+    expect(exported['configureBaseUrl']).toBeTruthy();
   });
 });
