6194965e81bd798326f169742d587306a6cdcf00
Lloyd Francis <lloydaf@gmail.com>
2020-10-19 21:18:53 +0200

    improvements: adding tests for removeTrailingCharacter

--- src/util/__tests__/string.util.test.ts
+++ src/util/__tests__/string.util.test.ts
@@ -5,4 +5,19 @@ describe('tests for string.util', () => {
     expect(exported['removeTrailingCharacter']).toBeTruthy();
     expect(exported['removeLeadingCharacter']).toBeTruthy();
   });
+
+  describe('removeTrailingCharacter', () => {
+    const removeTrailingCharacter = exported['removeTrailingCharacter'];
+    const sampleString = 'Hello';
+    const sampleCharacter = '+';
+    it('removes a trailing character if character present', () => {
+      const stringWithTrailingCharacter = sampleString + sampleCharacter;
+      const stringWithoutTrailingCharacter = removeTrailingCharacter(stringWithTrailingCharacter, sampleCharacter);
+      expect(stringWithoutTrailingCharacter).toEqual(sampleString);
+    });
+
+    it('does not alter the string if trailing character not present', () => {
+      expect(removeTrailingCharacter(sampleString, sampleCharacter)).toEqual(sampleString);
+    });
+  });
 });
